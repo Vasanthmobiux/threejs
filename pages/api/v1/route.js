5a3116bfbfb2ab6b276a81df1d196285e44526b0
Vasanthmobiux <vasanth@mobiux.in>
2024-03-17 17:08:48 +0530

    'fix'

--- pages/api/v1/route.js
+++ pages/api/v1/route.js
@@ -1,7 +1,7 @@
-const OpenAI = require("openai");
+import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export default async function handler(req, res) {
